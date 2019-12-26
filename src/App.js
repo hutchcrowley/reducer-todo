322c0aa5948e95d43102bddf4dcbaa1511e6700e
Hutch Crowley <tothecoda@gmail.com>
2019-12-25 23:18:02 -0600

    Update - localstorage stretch met

--- src/App.js
+++ src/App.js
@@ -1,53 +1,63 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from 'react'
+// Import the form and list components
+import TodoForm from '../src/components/TodoComponents/TodoForm'
+import TodoList from '../src/components/TodoComponents/TodoList'
+// Import custom hooks
+import { useInput } from './components/hooks/useInput'
+import { initialState, reducer } from './reducers/reducer'
 
-import TodoForm from "../src/components/TodoComponents/TodoForm";
-import TodoList from "../src/components/TodoComponents/TodoList";
-import { useInput } from "./components/hooks/useInput";
-
-import { initialState, reducer } from "./reducers/reducer";
-
-import "./App.css";
+import './App.css'
 
 // you will need a place to store your state in this component.
 // design `App` to be the parent component of your application.
 // this component is going to take care of state, and any change handlers you need to work with your state.
 
 const App = () => {
-  //  Use the useReducer hook, destructure two properties: state, dispatch. Pass in reducer and initialState to the function
-  const [todoList, dispatch] = useReducer(reducer, initialState);
-  const [todoInput, setTodoInput, handleTodoInput, value] = useInput([]);
+  //   Custom hook that handles input data
+  const [todoInput, setTodoInput, handleTodoInput, value] = useInput([])
 
-  console.log(initialState);
+  //  Calling the useReducer hook, taking the state of the todoList on first render, pulling the data from local storage if there is any, and if not, returning the initialState, which is a blank array, set in the reducer file.
+  const [todoList, dispatch] = useReducer(reducer, initialState, () => {
+    const localData = localStorage.getItem('todoList')
+    return localData ? JSON.parse(localData) : initialState
+  })
 
+  //   Using the useEffect hook to update the state when todoList is changed. Also, stringifying the todoList object and setting it to local storage.
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify(todoList))
+  }, [todoList])
+
+  console.log('Initial state in the App component: ', initialState)
+
+//   Function to handle adding a todo item.
   const handleSubmit = event => {
-    const value = event.target.todoInput.value;
-    if (todoInput !== "") {
+    const value = event.target.todoInput.value
+    if (todoInput !== '') {
       dispatch({
-        type: "ADD",
+        type: 'ADD',
         payload: value
-      });
-      setTodoInput("");
-      console.log(event.target.todoInput.value);
+      })
+      setTodoInput('')
     } else {
-      alert("Enter a new ToDo!");
+      alert('Enter a new ToDo!')
     }
-    event.preventDefault();
-  };
+    event.preventDefault()
+  }
 
   const clearAll = (todoList, initialState) => {
     if (todoList !== initialState) {
-      console.log(todoList);
+      console.log(todoList)
       dispatch({
-        type: "CLEAR_ALL",
+        type: 'CLEAR_ALL',
         payload: initialState
-      });
+      })
     }
-  };
+  }
 
   // Display the Todo List
   return (
-    <div className="App">
-      <div className="container">
+    <div className='App'>
+      <div className='container'>
         <h1>ToDo List:</h1>
         <TodoForm
           todoInput={todoInput}
@@ -60,7 +70,7 @@ const App = () => {
       </div>
       <TodoList todoList={todoList} dispatch={dispatch} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,30 +1,29 @@
-export const initialState = [];
+export const initialState = []
 
-// Reducer function: interacts with state and changes the data. 
+// Reducer function: interacts with state and changes the data.
 export const reducer = (state, action) => {
   switch (action.type) {
-    case "ADD":
-      return (
-        [...state,
-          {
+    case 'ADD':
+      return [
+        ...state,
+        {
           item: action.payload,
           completed: false,
-          id: Date.now(),
-          }
-        ]
-      );
+          id: Date.now()
+        }
+      ]
 
-    case "CLEAR":
-      return state.filter(todo => !todo.completed);
-    case "toggle":
+    case 'CLEAR':
+      return state.filter(todo => !todo.completed)
+    case 'toggle':
       return state.map(todo =>
         todo.id === action.payload
           ? { ...todo, completed: !todo.completed }
           : todo
-      );
-      case 'CLEAR_ALL':
-        return initialState
+      )
+    case 'CLEAR_ALL':
+      return initialState
     default:
-      return state;
+      return state
   }
-};
+}
